5008580ffe9d5048134df7bc35a7f8e999bb819b
2次曲线 <AdleyTales@126.com>
2018-04-04 17:01:17 +0800

    js

---
+++ getterFormData.js
@@ -0,0 +1,14 @@
+/**
+ * el：容器名 不需要form做容器
+ * 如果name的表单值为空，不会捕获
+ * 结果示例：{name:xxx,code:yyy}
+ */
+function getterFormData(el) {
+    var obj = {};
+    Array.from($(el).find('input,select,textarea')).forEach(function (item, index) {
+        if ($(item).attr('name') && $(item).val() != '') {
+            obj[$(item).attr('name')] = $(item).val();
+        }
+    });
+    return obj;
+}
